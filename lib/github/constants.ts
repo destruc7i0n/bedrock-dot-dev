9093daa51e3fb3b132806d55cfdabce507aad6ab
destruc7i0n <dscdsouza@outlook.com>
2020-05-27 11:45:12 -0400

    just use master

--- lib/github/constants.ts
+++ lib/github/constants.ts
@@ -6,5 +6,4 @@ export const RAW_GITHUB_URL = 'https://raw.githubusercontent.com'
 
 export const REPO_NAME = 'bedrock-dot-dev/docs'
 
-// use the github sha if possible
-export const REPO_TAG = process.env.VERCEL_GITHUB_COMMIT_SHA || 'master'
+export const REPO_TAG = 'master'
