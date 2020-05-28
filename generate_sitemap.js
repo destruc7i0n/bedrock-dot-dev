8c6e793b80ade6e21e08b90f883a9151593832eb
destruc7i0n <dscdsouza@outlook.com>
2020-05-28 11:12:45 -0400

    hopefully fix sitemap

--- generate_sitemap.js
+++ generate_sitemap.js
@@ -3,11 +3,15 @@ const fs = require('fs')
 
 const sitemap = require('nextjs-sitemap-generator')
 
-const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString();
+const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString()
+
+const nextPagesPath = process.env.VERCEL_GITHUB_DEPLOYMENT
+  ? '/.next/serverless/'
+  : path.join('/.next/server/static/', BUILD_ID)
 
 sitemap({
   baseUrl: 'https://bedrock.dev',
-  pagesDirectory: path.join(__dirname, '/.next/server/static/', BUILD_ID, '/pages'),
+  pagesDirectory: path.join(__dirname, nextPagesPath, '/pages'),
   targetDirectory : 'public/',
   ignoredPaths: ['[fallback]', 'api'],
 })
