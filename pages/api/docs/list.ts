709b0f396fccab6fc6b9d732a30acc624d2566d7
destruc7i0n <dscdsouza@outlook.com>
2020-05-31 16:59:26 -0400

    favicon

--- pages/api/docs/list.ts
+++ pages/api/docs/list.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { allFilesList } from 'lib/versions'
 
 export default async (_: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
+
   try {
     const files = await allFilesList()
     res.status(200).json({
--- scripts/generate_sitemap.js
+++ scripts/generate_sitemap.js
@@ -6,8 +6,8 @@ const sitemap = require('nextjs-sitemap-generator')
 const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString()
 
 const nextPagesPath = process.env.VERCEL_GITHUB_DEPLOYMENT
-  ? '/.next/serverless/'
-  : path.join('/.next/server/static/', BUILD_ID)
+  ? '../.next/serverless/'
+  : path.join('../.next/server/static/', BUILD_ID)
 
 sitemap({
   baseUrl: 'https://bedrock.dev',
