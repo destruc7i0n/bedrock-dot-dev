880e4122eaca42e2cf1b55f9f3ec0084d69e7293
destruc7i0n <dscdsouza@outlook.com>
2020-06-03 23:29:15 -0400

    better sitemap generation

--- scripts/generate_sitemap.js
+++ scripts/generate_sitemap.js
@@ -3,11 +3,18 @@ const fs = require('fs')
 
 const sitemap = require('nextjs-sitemap-generator')
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('sitemap.xml not generated')
+  process.exit(0)
+}
+
 const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString()
 
-const nextPagesPath = process.env.VERCEL_GITHUB_DEPLOYMENT
-  ? '../.next/serverless/'
-  : path.join('../.next/server/static/', BUILD_ID)
+const SERVERLESS_DIR = '../.next/serverless/'
+const STATIC_DIR = path.join('../.next/server/static/', BUILD_ID)
+
+let nextPagesPath = STATIC_DIR
+if (fs.existsSync(SERVERLESS_DIR)) nextPagesPath = SERVERLESS_DIR
 
 sitemap({
   baseUrl: 'https://bedrock.dev',
@@ -16,5 +23,5 @@ sitemap({
   ignoredPaths: ['[fallback]', 'api'],
 })
 
-console.log(`sitemap.xml generated!`);
+console.log(`sitemap.xml generated!`)
 
