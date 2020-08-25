08b2c8368236f923930d7bf70122f00cec152aaf
destruc7i0n <destruc7i0n@users.noreply.github.com>
2020-08-25 13:28:40 -0400

    revert that

--- lib/html/clean.ts
+++ lib/html/clean.ts
@@ -1,5 +1,3 @@
-import cheerio from 'cheerio'
-
 import {
   MARKDOWN_CODE_MATCH,
   STYLESHEET_MATCH,
@@ -11,15 +9,15 @@ import { oneLine } from '../util'
 import { addAnchors } from './scrape/sections'
 
 export const cleanHtmlForDisplay = (html: string, file: string, version: string) => {
-  html = encloseDocumentationText(html, file, version)
   html = removeDisplayHtml(html)
+  html = encloseDocumentationText(html, file, version)
   html = addAnchors(html)
 
   return html
 }
 
 // add <p> tags around elements
-const encloseDocumentationTextRegex = (html: string) => {
+const encloseDocumentationText = (html: string, file: string, _version: string) => {
   // debug helper [...document.getElementsByTagName('p')].forEach(e => e.style.background = 'red')
   // match stuff between headings
   html = html.replace(/<\/h[1-4]>([^]*?)(?:<h[1-4]|$)/g, (match: string, el: string) => {
@@ -35,90 +33,26 @@ const encloseDocumentationTextRegex = (html: string) => {
 
     for (let match of trimmed.split(bothMatches)) {
       const single = oneLine(match)
-      if (ONLY_NEWLINES_AND_WHITESPACE.test(single)) continue
+      if (ONLY_NEWLINES_AND_WHITESPACE.test(single) || !match.trim().length) continue
+      // don't enclose the schemas
+      if (file === 'Schemas' && MARKDOWN_CODE_MATCH.test(match)) continue
 
-      newEl = newEl.replace(match, `<p>${match}</p>`)
+      let content = match
+      if (file === 'MoLang') {
+        // replace any elements mistaken for html tags in ``
+        content = content.replace(/`(.*?)`/g, (_, group: string) => {
+          // remove the <> around them
+          return '`' + group.replace(/<([\w\s]+)>/g, (_, group: string) => group) + '`'
+        })
+      }
+
+      newEl = newEl.replace(match, `<p>${content}</p>`)
     }
 
     return match.replace(el, newEl)
   })
 
   return html
-}
-
-// add <p> tags around elements
-export const encloseDocumentationText = (html: string, file: string, version: string): string => {
-  // 1.9.0.10 uses the <> very often so I'll just use the regex method
-  if (version === '1.9.0.10') return encloseDocumentationTextRegex(html)
-
-  // use cheerio just here since it is simpler than regex
-  // hopefully I do not need to use it again..
-
-  let schemaContent = ''
-  switch (file) {
-    case 'Animations': {
-      // these are tags that cheerio mistakes
-      html = html
-        .replace('<entity_name>', 'entity_name')
-        .replace('<name>', 'name')
-        .replace('<resource_pack_root>', 'resource_pack_root')
-      break
-    }
-    case 'Schemas': {
-      // copy the markdown stuff to prevent any changes from happening to it
-      const markdownCode = html.match(MARKDOWN_CODE_MATCH)
-      if (markdownCode) {
-        schemaContent = markdownCode[1]
-      }
-      break
-    }
-    case 'MoLang': {
-      // remove the <> around
-      const escape = (s: string) => s.replace(/<([\w\s]+)>/g, (_, group: string) => group)
-
-      // 1.16.100 docs use `` and have html tags in them, fix them
-      html = html.replace(/`(.*?)`/g, (_, group: string) => '`' + escape(group) + '`')
-      break
-    }
-  }
-
-  const $ = cheerio.load(html, { decodeEntities: false })
-  const body = $('body')
-
-  const expectedTags = [ 'br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'textarea', 'table', 'tr', 'td', 'pre' ]
-  // wrap all text which is not wrapped
-  const textBlocks = body.contents().filter(function (_, el) {
-    const node = $(el).get(0)
-
-    // alert me of any new tags which may have appeared
-    if (node.tagName && !expectedTags.includes(node.tagName.toLowerCase())) {
-      if (file !== 'Schemas')
-        console.log(`\nFound unexpected tag "${node.tagName}" on page "${file}" (${version})`)
-    }
-
-    return node.nodeType === 3 && node.nodeValue.trim() !== '' && node.tagName === null
-  })
-
-  // remove `br` tags around to prevent a lot of space
-  textBlocks.each(function (_, el) {
-    const cheerioEl = $(el)
-    const nextTag = cheerioEl.next().get(0).tagName
-    if (nextTag.toLowerCase() === 'br') cheerioEl.next().remove()
-  })
-
-  textBlocks.wrap('<p/>')
-
-  let updatedHtml = body.html()!
-
-  // re-add the markdown stuff after parsing
-  switch (file) {
-    case 'Schemas': {
-      if (schemaContent) updatedHtml = updatedHtml.replace(MARKDOWN_CODE_MATCH, schemaContent)
-      break
-    }
-  }
-
-  return updatedHtml
 }
 
 // remove any html from the old generator
