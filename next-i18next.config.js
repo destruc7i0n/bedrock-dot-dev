65c27fb7a8d06bada2a74be118ed81b65a93b476
destruc7i0n <dscdsouza@outlook.com>
2021-02-03 11:18:07 -0500

    only when not in ci

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
 
 module.exports = {
-  localePath: process.env.CI ? path.resolve('./locales') : path.resolve('./public/locales'),
+  localePath: (process.env.VERCEL && !process.env.CI) ? path.resolve('./locales') : path.resolve('./public/locales'),
 }
