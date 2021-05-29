0e7bb3b7a8f7fe91bf64ab4641ff668ad355b0e5
destruc7i0n <destruc7i0n@users.noreply.github.com>
2021-05-28 23:43:02 -0400

    show document file count in build logs

--- scripts/generate-docs-manifest.ts
+++ scripts/generate-docs-manifest.ts
@@ -9,6 +9,16 @@ import { getVersionsFile } from '../lib/versions'
 const main = async () => {
   const file = await getVersionsFile()
 
+  // count the number of documentation files per locale
+  for (let [locale, versions] of Object.entries(file['versions'])) {
+    let count = 0
+    for (let [, minorVersions] of Object.entries(versions)) {
+      // sum the number of files per version
+      count += Object.values(minorVersions).reduce((acc, files) => acc + files.length, 0)
+    }
+    console.log(`found ${count} ${locale.toUpperCase()} documentation files`)
+  }
+
   if (!fs.existsSync('public/static')) fs.mkdirSync('public/static')
 
   const dir = path.resolve('public/static/docs.json')
