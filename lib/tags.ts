85962fa5300efd95787eb3a9e9e70848ae638b99
destruc7i0n <destruc7i0n@users.noreply.github.com>
2022-10-25 11:41:09 -0400

    ci is undefined

--- lib/tags.ts
+++ lib/tags.ts
@@ -14,12 +14,12 @@ export type TagsResponse = {
 }
 // fetch the tags file from the repository
 export const getTags = async (locale: Locale): Promise<TagsResponse> => {
-  // fetch the tags from the server
-  if (process.env.NODE_ENV === 'production' && process.env.CI !== 'true') {
-    // fetch from cached file
+  if (process.env.NODE_ENV === 'production' && process.env.CI === undefined) {
+    // fetch from cached file if on server
     const tags = require('../public/static/tags.json')
     return tags
   } else {
+    // fetch the tags from the server
     const repo = getRepository(locale)
     const tags = await fetch(`${RAW_GITHUB_URL}/${repo.name}/${repo.tag}/tags.json`)
     return await tags.json()
