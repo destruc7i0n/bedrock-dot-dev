ef5ac8a14afb8215b8b8a1bb4116db44fc5febbb
destruc7i0n <destruc7i0n@users.noreply.github.com>
2023-01-02 14:31:12 -0500

    packs page provide bedrock-samples links

--- lib/github/api.ts
+++ lib/github/api.ts
@@ -11,6 +11,13 @@ export interface GitHubTreeResponse {
   }[]
 }
 
+export interface GithubReleasePartial {
+  tag_name: string
+  name: string
+  published_at: string
+  html_url: string
+}
+
 async function getError(res: Response): Promise<Error> {
   const errorText = await getErrorText(res)
   return new Error(`GitHub api error (${res.status}): ${errorText}`)
@@ -24,4 +31,26 @@ export async function listAllFilesFromRepo (locale: Locale): Promise<GitHubTreeR
 
   if (res.ok) return res.json()
   return getError(res)
+}
+
+// list all releases from the repo from all time, paginating if necessary
+export async function listReleases (repo: string): Promise<GithubReleasePartial[]> {
+  let releases: GithubReleasePartial[] = []
+  let page = 1
+
+  while (true) {
+    // https://api.github.com/repos/bedrock-dot-dev/docs/releases?page=1
+    const res = await fetch(`${GITHUB_API_URL}/repos/${repo}/releases?page=${page}&per_page=100`)
+
+    if (res.ok) {
+      const json = await res.json()
+      if (json.length === 0) break
+      releases = [...releases, ...json]
+      page++
+    } else {
+      return []
+    }
+  }
+
+  return releases
 }
--- pages/packs.tsx
+++ pages/packs.tsx
@@ -1,59 +1,24 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, { FunctionComponent } from 'react'
 import { GetStaticProps } from 'next'
 
 import { useTranslation, Trans } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import { ArrowDownTrayIcon } from '@heroicons/react/20/solid'
-
 import S3 from 'aws-sdk/clients/s3'
 
-import Log from '../lib/log'
-import { compareBedrockVersions } from '../lib/util'
-import { getLocale } from '../lib/i18n'
+import Log from 'lib/log'
+import { compareBedrockVersions } from 'lib/util'
+import { getLocale } from 'lib/i18n'
+import { listReleases } from 'lib/github/api'
+import { VERSION } from 'lib/html/regex'
 
 import Layout from 'components/layout'
 import Navbar from 'components/homepage/navbar'
 import Footer from 'components/footer'
+import PackCard from 'components/packs/pack-card'
 
-const getUrl = (folder: string, id: string) => {
-  return ['https://void.bedrock.dev', folder, `${id}.zip`].join('/')
-}
-
-type PackCardProps = {
-  version: string
-  versions: PackVersions
-}
-
-const PackCard: FunctionComponent<PackCardProps> = ({ version, versions }) => {
-  const { t } = useTranslation('common')
-
-  const [open, setOpen] = useState(false)
-
-  const links = (
-    <>
-      {versions[version][0] ? (
-        <a href={getUrl('behaviours', version)} className='link' download>{t('component.packs_page.behaviours_link')}</a>
-      ) : (
-        <p className='text-gray-500 dark:text-gray-400'>{t('component.packs_page.behaviours_link')}</p>
-      )}
-
-      {versions[version][1] ? (
-        <a href={getUrl('resources', version)} className='link' target='_blank' download rel='noopener'>{t('component.packs_page.resources_link')}</a>
-      ) : (
-        <p className='text-gray-500 dark:text-gray-400'>{t('component.packs_page.resources_link')}</p>
-      )}
-    </>
-  )
-
-  return (
-    <div className='text-center bg-gray-50 dark:bg-dark-gray-950 border border-gray-200 dark:border-transparent p-2 rounded-md'>
-      <p className='text-lg dark:text-gray-200'>{version}</p>
-      <div className='flex flex-col w-full md:flex-row md:justify-around items-center justify-center'>
-        {open ? links : <span className='link cursor-pointer' onClick={() => setOpen(!open)}><ArrowDownTrayIcon className='pointer-events-none w-6 h-6' /></span>}
-      </div>
-    </div>
-  )
+export type PackVersions = {
+  [key: string]: Partial<{ b: boolean, r: boolean, git: string }>
 }
 
 type PacksPageProps = {
@@ -62,7 +27,7 @@ type PacksPageProps = {
 
 const PacksPage: FunctionComponent<PacksPageProps> = ({ versions }) => {
   const { t } = useTranslation('common')
-  const ordered = Object.keys(versions).sort(compareBedrockVersions)
+  const versionsSorted = Object.keys(versions).sort(compareBedrockVersions)
 
   return (
     <Layout title={`${t('page.packs.website_title')} | bedrock.dev`} description={t('page.packs.website_description')}>
@@ -73,11 +38,14 @@ const PacksPage: FunctionComponent<PacksPageProps> = ({ versions }) => {
           <div className='max-w-screen-lg mx-auto space-y-4'>
             <h4 className='text-4xl font-bold text-black dark:text-gray-200'>{t('page.packs.title')}</h4>
             <p className='text-lg text-black dark:text-gray-200'>
-              <Trans i18nKey='page.packs.subtitle' t={t} components={[<a className='link' href='https://github.com/bedrock-dot-dev/packs' target='_blank' />]} />
+              <Trans i18nKey='page.packs.subtitle' t={t} components={[
+                <a className='link' href='https://github.com/bedrock-dot-dev/packs' target='_blank' />,
+                <a className='link' href='https://github.com/Mojang/bedrock-samples?ref=bedrock.dev' target='_blank' />,
+              ]} />
             </p>
 
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 font-normal'>
-              {ordered.map((v, i) => <PackCard key={`packs-versions-${i}`} version={v} versions={versions} />)}
+              {versionsSorted.map((v, i) => <PackCard key={`packs-versions-${i}`} versionName={v} versionData={versions[v]} />)}
             </div>
           </div>
         </div>
@@ -88,10 +56,6 @@ const PacksPage: FunctionComponent<PacksPageProps> = ({ versions }) => {
       </div>
     </Layout>
   )
-}
-
-type PackVersions = {
-  [key: string]: [boolean, boolean]
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale: localeVal }) => {
@@ -118,10 +82,17 @@ export const getStaticProps: GetStaticProps = async ({ locale: localeVal }) => {
     const [ folder, name ] = path.split('/')
     if (folder && name) {
       const version = name.replace('.zip', '')
-      if (!versions[version]) versions[version] = [false, false]
-      if (folder === 'behaviours') versions[version][0] = true
-      if (folder === 'resources') versions[version][1] = true
+      if (!versions[version]) versions[version] = { b: false, r: false }
+      if (folder === 'behaviours') versions[version].b = true
+      if (folder === 'resources') versions[version].r = true
     }
+  }
+
+  // get the releases from the bedrock-samples repo
+  const mojangBedrockSamples = await listReleases('Mojang/bedrock-samples')
+  for (const release of mojangBedrockSamples) {
+    const version = release.tag_name.match(VERSION)?.[0]
+    if (version && !versions[version]) versions[version] = { git: release.html_url }
   }
 
   return { props: { versions, ...(await serverSideTranslations(locale, ['common'])) } }
