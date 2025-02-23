dd1d9a53cfa35667603994210b9d2617eee04e0f
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-02-22 17:28:23 -0800

    fix application error

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,13 @@
-const IS_VERCEL = !!process.env.NEXT_PUBLIC_VERCEL_URL;
 const BEDROCK_DEV_URL = "https://bedrock.dev";
 
-export const VERCEL_URL = IS_VERCEL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "";
+const VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development";
+const PUBLIC_VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+const IS_VERCEL = Boolean(PUBLIC_VERCEL_URL);
+
+export const VERCEL_URL = IS_VERCEL ? `https://${PUBLIC_VERCEL_URL}` : "";
 
 export const LIVE_URL = IS_VERCEL
-  ? process.env?.NEXT_PUBLIC_VERCEL_ENV === "production"
+  ? VERCEL_ENV === "production"
     ? BEDROCK_DEV_URL
     : VERCEL_URL
   : BEDROCK_DEV_URL; // fallback to bedrock.dev if local
