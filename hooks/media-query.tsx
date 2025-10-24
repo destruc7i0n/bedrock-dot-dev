9a7ce804a63d059ff4b8e08eb91f20e501c99f04
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-10-24 16:51:20 -0700

    some cleanup

--- hooks/media-query.tsx
+++ hooks/media-query.tsx
@@ -2,24 +2,19 @@ import { useState, useCallback, useEffect } from "react";
 
 // from https://github.com/zeit/next-site/blob/master/components/media-query.js
 
-const getMediaQuery = (width: number) => {
-  return (
-    (window.matchMedia && window.matchMedia(`(max-width: ${width}px)`)) || {
-      matches: false,
-    }
-  );
+const getMediaQuery = (width: number): MediaQueryList | undefined => {
+  if (typeof window === "undefined") {
+    return undefined;
+  }
+  return window.matchMedia && window.matchMedia(`(max-width: ${width}px)`);
 };
 
-const isLg = () => {
-  if (typeof window === 'undefined') return true;
-  return !getMediaQuery(1024).matches;
-};
+const isLg = () => getMediaQuery(1024)?.matches ?? false;
 
 const useMediaQuery = (width: number) => {
-  const [targetReached, setTargetReached] = useState(() => {
-    if (typeof window === 'undefined') return false;
-    return getMediaQuery(width).matches;
-  });
+  const [targetReached, setTargetReached] = useState(
+    () => getMediaQuery(width)?.matches ?? false,
+  );
 
   const updateTarget = useCallback((e: MediaQueryListEvent) => {
     if (e.matches) {
@@ -31,9 +26,11 @@ const useMediaQuery = (width: number) => {
 
   useEffect(() => {
     const media = getMediaQuery(width);
-    media.addEventListener('change', updateTarget);
+    if (!media) return;
 
-    return () => media.removeEventListener('change', updateTarget);
+    media.addEventListener("change", updateTarget);
+
+    return () => media.removeEventListener("change", updateTarget);
   }, [updateTarget, width]);
 
   return targetReached;
