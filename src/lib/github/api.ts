fa4be8d2f9ffa2fa55d0b468057d0d87c02c07dc
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-11-09 13:31:15 -0800

    set installCommand

--- src/lib/github/api.ts
+++ src/lib/github/api.ts
@@ -9,7 +9,7 @@ export interface GithubReleasePartial {
 
 // list all releases from the repo from all time, paginating if necessary
 export async function listReleases(
-  repo: string
+  repo: string,
 ): Promise<GithubReleasePartial[]> {
   let releases: GithubReleasePartial[] = [];
   let page = 1;
@@ -17,7 +17,7 @@ export async function listReleases(
   while (true) {
     // https://api.github.com/repos/bedrock-dot-dev/docs/releases?page=1
     const res = await fetch(
-      `${GITHUB_API_URL}/repos/${repo}/releases?page=${page}&per_page=100`
+      `${GITHUB_API_URL}/repos/${repo}/releases?page=${page}&per_page=100`,
     );
 
     if (res.ok) {
