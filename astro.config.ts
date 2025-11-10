2c439b63a70583a96db5592a0296b2c1d8a0ce67
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-11-09 19:30:11 -0800

    fix package.json

--- astro.config.ts
+++ astro.config.ts
@@ -3,14 +3,13 @@ import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
-import sitemap from "@astrojs/sitemap";
 
 import nprogress from "astro-nprogress";
 
 export default defineConfig({
   site: "https://bedrock.dev",
   prefetch: true,
-  integrations: [tailwind(), react(), nprogress(), sitemap()],
+  integrations: [tailwind(), react(), nprogress()],
   adapter: vercel(),
   build: {
     concurrency: 10,
