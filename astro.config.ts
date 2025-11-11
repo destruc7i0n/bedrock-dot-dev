7530a8e80e09cf8fbdaa04d2312b511c3e5c52d9
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-11-10 21:58:37 -0800

    use astro redirects

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, fontProviders } from "astro/config";
+import type { RedirectConfig } from "astro";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -6,11 +7,50 @@ import vercel from "@astrojs/vercel";
 
 import nprogress from "astro-nprogress";
 
+import { getTags } from "./src/lib/tags";
+import { Locale } from "./src/lib/i18n";
+
+async function generateRedirects(): Promise<Record<string, RedirectConfig>> {
+  const { stable, beta } = await getTags(Locale.English);
+
+  return {
+    "/docs/stable": {
+      status: 302,
+      destination: `/?r=${stable.join("/")}`,
+    },
+    "/docs/beta": {
+      status: 302,
+      destination: `/?r=${beta.join("/")}`,
+    },
+    "/r/MoLang": {
+      status: 302,
+      destination: "/docs/stable/Molang",
+    },
+    "/r/[...slug]": "/docs/stable/[...slug]",
+    "/r": {
+      status: 302,
+      destination: `/?r=${stable.join("/")}`,
+    },
+    "/b/MoLang": {
+      status: 302,
+      destination: "/docs/beta/Molang",
+    },
+    "/b/[...slug]": "/docs/beta/[...slug]",
+    "/b": {
+      status: 302,
+      destination: `/?r=${beta.join("/")}`,
+    },
+  };
+}
+
+const redirects = await generateRedirects();
+
 export default defineConfig({
   site: "https://bedrock.dev",
   prefetch: true,
   integrations: [tailwind(), react(), nprogress()],
   adapter: vercel(),
+  redirects,
   build: {
     concurrency: 10,
   },
